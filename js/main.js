a5d69d7f68f76d3785bfc4edc9d93a8a9f6e0cc2
Ariel Paixão Dos Santos <33747821839@info.local>
2018-09-10 17:27:32 -0300

    Responsividade

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function () {
     $(window).on("scroll", function () {
-        if ($(window).scrollTop() > 500) {
+        if ($(window).scrollTop() > 1) {
             $(".logo").addClass("small-width");
             $(".logo img").addClass("small-height");
 
@@ -15,3 +15,14 @@ $(function () {
         }
     });
 });
+
+// Toggle hide bar
+
+var menu_btn = document.getElementById("menuBtn");
+var menu = document.getElementById("menuTop");
+// var redes = document.getElementById("logoRedesSociais");
+
+menu_btn.onclick = function() {
+    menu.classList.toggle("hide");
+    // redes.classList.toggle("hide");
+}
